5c06d960bc59a0d23d71b8615f8dbbfea19fbef0
Nicolás Calvache <nicolas.calvache@alumnos.uneatlantico.es>
2020-12-29 17:51:30 +0100

    finished tema 7.1 prior to check

---
+++ tema_7/src/edai/tema7/ejercicio1/Program.java
@@ -0,0 +1,117 @@
+package edai.tema7.ejercicio1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Program {
+
+	public static void main(String[] args) {
+		String file1 = "D:\\1Main\\Libraries\\Documents\\Uni\\2020-2021\\Semestre 1\\EDA I\\Practicas\\tema_7\\src\\edai\\tema7\\ejercicio1\\numeros1";
+		String file2 = "D:\\1Main\\Libraries\\Documents\\Uni\\2020-2021\\Semestre 1\\EDA I\\Practicas\\tema_7\\src\\edai\\tema7\\ejercicio1\\numeros2";
+
+		int[] file1Numbers = castArr(readFile(file1));
+		int[] file2Numbers = castArr(readFile(file2));
+
+		int[] arrWithoutDuplicates = deleteDuplicates(Sorter.bubbleSort(mergeArr(file1Numbers, file2Numbers)));
+
+		int[] finalArray = invertArr(arrWithoutDuplicates);
+
+		System.out.println(Arrays.toString(Sorter.bubbleSort(mergeArr(file1Numbers, file2Numbers))));
+		System.out.println(Arrays.toString(arrWithoutDuplicates));
+		System.out.println(Arrays.toString((finalArray)));
+
+		createTxt(finalArray);
+
+	}
+
+	private static String[] readFile(String file) {
+		StringBuffer sb = null;
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(file));
+
+			sb = new StringBuffer();
+			int c;
+			while ((c = in.read()) != -1) {
+				sb.append((char) c);
+			}
+
+			in.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString().split("\\r\\n");
+	}
+
+	private static int[] castArr(String[] arr) {
+		int[] castedArr = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			castedArr[i] = Integer.parseInt(arr[i]);
+		}
+		return castedArr;
+	}
+
+	private static int[] mergeArr(int[] a, int[] b) {
+		int a1 = a.length;
+		int b1 = b.length;
+		int[] c = new int[a1 + b1];
+
+		System.arraycopy(a, 0, c, 0, a1);
+		System.arraycopy(b, 0, c, a1, b1);
+		return c;
+	}
+
+	@SuppressWarnings("unused")
+	private static int[] deleteDuplicates(int[] arr) {
+		ArrayList<Integer> newArr = new ArrayList<Integer>();
+		int arrLen = arr.length;
+		for (int i = 0; i < arrLen; i++) {
+			for (int j = i + 1; j < arrLen; j++) {
+				if (arr[i] == arr[j]) {
+					break;
+				} else {
+					newArr.add(arr[i]);
+					break;
+				}
+			}
+		}
+
+		int[] aux = new int[newArr.size()];
+
+		for (int i = 0; i < newArr.size(); i++) {
+			if (newArr.get(i) != null) {
+				aux[i] = newArr.get(i);
+			}
+		}
+
+		return aux;
+	}
+
+	private static int[] invertArr(int[] arr) {
+		int[] auxArr = new int[arr.length];
+		int j = 0;
+		for (int i = arr.length - 1; j < arr.length; i--, j++) {
+			auxArr[j] = arr[i];
+		}
+		return auxArr;
+	}
+
+	private static void createTxt(int[] data) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(
+					"D:\\\\1Main\\\\Libraries\\\\Documents\\\\Uni\\\\2020-2021\\\\Semestre 1\\\\EDA I\\\\Practicas\\\\tema_7\\\\src\\\\edai\\\\tema7\\\\ejercicio1\\\\resultado"));
+			for (int i = 0; i < data.length; i++) {
+				out.write(Integer.toString(data[i]));
+				out.newLine();
+			}
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ tema_7/src/edai/tema7/ejercicio1/Sorter.java
@@ -0,0 +1,24 @@
+package edai.tema7.ejercicio1;
+
+public class Sorter {
+	public static int[] bubbleSort(int[] numbers) {
+		int arrLength = numbers.length;
+		int aux;
+		boolean swapped;
+		for (int i = 0; i < arrLength - 1; i++) {
+			swapped = false;
+			for (int j = 0; j < arrLength - i - 1; j++) {
+				if (numbers[j] > numbers[j + 1]) {
+					aux = numbers[j];
+					numbers[j] = numbers[j + 1];
+					numbers[j + 1] = aux;
+					swapped = true;
+				}
+			}
+			if (swapped == false) {
+				break;
+			}
+		}
+		return numbers;
+	}
+}
