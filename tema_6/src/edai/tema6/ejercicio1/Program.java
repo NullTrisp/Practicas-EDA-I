4b5fc0212b6e23a74939f417c4b4c597c4f158b5
Nicolás Calvache <nicolas.calvache@alumnos.uneatlantico.es>
2020-12-29 15:56:34 +0100

    finished tema 6

--- tema_6/src/edai/tema6/ejercicio1/Program.java
+++ tema_6/src/edai/tema6/ejercicio1/Program.java
@@ -1,5 +1,8 @@
 package edai.tema6.ejercicio1;
 
+import java.util.ArrayList;
+
+import org.graphstream.algorithm.Dijkstra;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
@@ -21,30 +24,33 @@ public class Program {
 		graph.addNode("G");
 		graph.addNode("Z");
 		graph.addEdge("AB", "A", "B").setAttribute("length", 4);
-		graph.addEdge("AG", "A", "G");
-		graph.addEdge("AD", "A", "D");
-		graph.addEdge("BG", "B", "G");
-		graph.addEdge("BC", "B", "C");
-		graph.addEdge("DG", "D", "G");
-		graph.addEdge("DE", "D", "E");
-		graph.addEdge("EF", "E", "F");
-		graph.addEdge("EZ", "E", "Z");
-		graph.addEdge("CF", "C", "F");
-		graph.addEdge("CZ", "C", "Z");
-		graph.addEdge("FZ", "F", "Z");
+		graph.addEdge("AG", "A", "G").setAttribute("length", 2);
+		graph.addEdge("AD", "A", "D").setAttribute("length", 5);
+		graph.addEdge("BG", "B", "G").setAttribute("length", 1);
+		graph.addEdge("BC", "B", "C").setAttribute("length", 5);
+		graph.addEdge("DG", "D", "G").setAttribute("length", 1);
+		graph.addEdge("DE", "D", "E").setAttribute("length", 2);
+		graph.addEdge("EF", "E", "F").setAttribute("length", 1);
+		graph.addEdge("EZ", "E", "Z").setAttribute("length", 3);
+		graph.addEdge("CF", "C", "F").setAttribute("length", 1);
+		graph.addEdge("CZ", "C", "Z").setAttribute("length", 1);
+		graph.addEdge("FZ", "F", "Z").setAttribute("length", 2);
 
 		return graph;
 
 	}
 
 	public static String[] createMatrix(Graph graph) {
-		String[] nodesArr = new String[graph.getNodeCount()];
-		int i = 0;
+		Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE, null, "length");
+		ArrayList<String> path = new ArrayList<String>();
+		dijkstra.init(graph);
+		dijkstra.setSource(graph.getNode("A"));
+		dijkstra.compute();
+		String lastNode;
 		for (Node node : graph) {
-			//nodesArr[i] = node.toString() + " " + "(",;
-			++i;
+			lastNode = (dijkstra.getParent(node) == null) ? "-" : dijkstra.getParent(node).getId();
+			path.add(node.getId() + " (" + (int) dijkstra.getPathLength(node) + ", " + lastNode + ")");
 		}
-		return nodesArr;
-
+		return path.toArray(new String[path.size()]);
 	}
 }
